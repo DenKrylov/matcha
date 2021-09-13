1878d472b9f6981424a1fffbb9dfd00bc8feb928
DenKrylov <deniskrilov87@gmail.com>
2021-09-13 11:24:58 +0300

    add vue

--- frontend/script/app.js
+++ frontend/script/app.js
@@ -1,3 +1,26 @@
 new Vue ({
-	el: '.app'
+	el: '.app',
+	data: {
+		mainPage: true,
+		loginPage: false,
+		chatPage: false,
+		user: 'Денис',
+	},
+	methods: {
+		showMain: function() {
+			this.mainPage = true;
+			this.loginPage = false;
+			this.chatPage = false;
+		},
+		showLogin: function() {
+			this.mainPage = true;
+			this.loginPage = true;
+			this.chatPage = false;
+		},
+		showChat: function() {
+			this.mainPage = false;
+			this.loginPage = false;
+			this.chatPage = true;
+		},
+	}
 })
